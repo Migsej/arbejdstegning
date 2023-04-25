c5d6205e34401fef345457373c475c676b583d70
christian ahrnkiel <clatre28@gmail.com>
2023-04-25 13:18:49 +0200

    real measurements

--- main.py
+++ main.py
@@ -4,12 +4,28 @@ import numpy as np
 
 LAYERS = 58
 
+HEIGHT = 250
+
+def pretyprint(liste):
+    for element in liste:
+        element = round_down(element,1)
+        print(str(element)+" mm")
+
+
+def round_down(value, decimals):
+    import math
+    return math.floor(value * 100)/100.0
+
+
 def main():
      
     # Load an color image in grayscale
-    img = cv2.imread('/home/vincent/arbejdstegning/billede.png',0)
+    img = cv2.imread('billede.png',0)
     (thresh, blackwhite) = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+    dimensions = blackwhite.shape
+    print(dimensions) 
     
+    forhold = HEIGHT/dimensions[1]
     
     width = len(blackwhite) // LAYERS
 
@@ -18,10 +34,11 @@ def main():
         newarray = blackwhite[width*i:width*i+width]
         white = np.count_nonzero(newarray==255)
         black = np.count_nonzero(newarray==0)
-        result.append((black//width)/len(blackwhite[0]))
+        element = (black//width)/len(blackwhite[0])
+        result.append(element*dimensions[0]*forhold/2)
     
         
-    print(result)
+    pretyprint(result)
     # show image
     cv2.imshow('image',blackwhite)
     cv2.waitKey(0)
@@ -29,3 +46,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
